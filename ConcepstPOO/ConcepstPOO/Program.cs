a2e6fde272d5c9d47c31083b81a8f28e0d31f64e
MOITEER <moiteer@hotmail.com>
2023-03-02 20:27:45 -0600

    Finish Date Class

---
+++ ConcepstPOO/ConcepstPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConcepstPOO;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("=============");
+Date date1 = new Date(2022, 05, 25);
+
+try
+{
+    Console.WriteLine(new Date(2022, 12, 7));
+    Console.WriteLine(new Date(2025, 25, 55));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
